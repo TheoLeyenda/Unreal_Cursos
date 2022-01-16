4813b2a51501b006aaaa021757643047b919b566
Theo Leyenda <theoleyenda1997@gmail.com>
2022-01-16 15:50:48 -0300

    Refactoreo del GameMode en progreso...

--- Curso_1/Intermediate/Build/Win64/Curso_1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Curso_1/Intermediate/Build/Win64/Curso_1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30138
-#include "C:/Users/Theo/Desktop/Unreal_Cursos/Curso_1/Intermediate/Build/Win64/Curso_1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.29.30136
+#include "C:/Users/theof/Desktop/Unreal_Cursos/Curso_1/Intermediate/Build/Win64/Curso_1Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
