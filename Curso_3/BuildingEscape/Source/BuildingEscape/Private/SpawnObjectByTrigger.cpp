b3e410d5764aa2d987b022b76fdb92f3d0fa8150
Theo Leyenda <theoleyenda1997@gmail.com>
2022-01-31 23:15:10 -0300

    Update Files and Finish SpawnObjectByTrigger

--- Curso_3/BuildingEscape/Source/BuildingEscape/Private/SpawnObjectByTrigger.cpp
+++ Curso_3/BuildingEscape/Source/BuildingEscape/Private/SpawnObjectByTrigger.cpp
@@ -8,10 +8,15 @@ ASpawnObjectByTrigger::ASpawnObjectByTrigger() : AObjectsInteractTrigger(){}
 bool ASpawnObjectByTrigger::ExecuteInteractInterface()
 {
 	//ACA HACER QUE SE SPAWNEEN LOS OBJETOS SI SE ENCUENTRA EL SPAWNER.
+
+	if(!ObjectSpawner){return false;}
+
+	ObjectSpawner->SendTimerSpawnObject();
 	
 	if(bDestroymeInTrigger)
 	{
 		Destroy();
 	}
+	
 	return true;
 }
